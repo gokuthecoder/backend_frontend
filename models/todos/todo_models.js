214e8e3ead035da8c80e7c2832346522319b9f22
gokuthecoder <vishalkr12012001@gmail.com>
2024-02-05 00:48:48 +0530

    3 models added

--- models/todos/todo_models.js
+++ models/todos/todo_models.js
@@ -1,5 +1,28 @@
 import mongoose from "mongoose";
 
-const todoSchema = new mongoose.Schema({}, { timestamps: true })
+const todoSchema = new mongoose.Schema({
+    content: { 
+        type: String,
+        required: true,
+    },
+    complete: {
+        type: Boolean,
+        default:false
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    subTodods: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "SubTodo"
+        }
+    ]
+}, { timestamps: true })
 
-export const Todo = mongoose.model('Todo',todoSchema) // mongo ask tell me what is keep name of data name , i say Todo, mongo say, okay which schema based make your database , i say todoSchema
+export const Todo = mongoose.model('Todo', todoSchema) 
+
+// mongo ask tell me what is keep name of data name ,
+ //i say Todo, mongo say, okay which schema based 
+ //make your database , i say todoSchema
